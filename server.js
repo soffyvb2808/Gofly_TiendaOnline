4ad21ba29ea63838fb57d3b48fb0a64b1de5d9d1
soffyvb2808 <soffyvb2808@gmail.com>
2025-08-25 20:58:56 -0500

    Primer deploy

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/formulario", (req, res) => {
+  res.sendFile(path.join(__dirname, "formulario.html"));
+});
+
+app.get("/galeria", (req, res) => {
+  res.sendFile(path.join(__dirname, "galeria.html"));
+});
+
+app.get("/precios", (req, res) => {
+  res.sendFile(path.join(__dirname, "precios.html"));
+});

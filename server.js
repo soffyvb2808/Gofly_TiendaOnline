0a10a2c0c6bff6db9716c38a81a6bc82b8232806
soffyvb2808 <soffyvb2808@gmail.com>
2025-08-25 21:34:03 -0500

    Agrego app.listen para Render

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
 
 app.use(cors());
 app.use(express.json());
